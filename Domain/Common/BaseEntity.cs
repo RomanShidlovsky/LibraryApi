a86c779c18137710383de6adf988fff05c64ef18
Shidlovsky Roman <shidlovskyroman@gmail.com>
2024-01-19 15:59:27 +0300

    Initial commit

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTimeOffset DateCreated { get; set; }
+    public DateTimeOffset? DateUpdated { get; set; }
+}

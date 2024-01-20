fab9f5e03631472edd04a438e24306335d191dc0
Shidlovsky Roman <shidlovskyroman@gmail.com>
2024-01-20 17:06:14 +0300

    Implement wrappers

---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,27 @@
+﻿using Application.Exceptions;
+
+namespace Application.Wrappers;
+
+public class Response<T>
+{
+    public bool Succeeded { get; set; }
+    public string Message { get; set; }
+    public List<ErrorModel> Errors { get; set; }
+    public T Data { get; set; }
+    
+    public Response() { }
+
+    public Response(T data, string message = "")
+    {
+        Succeeded = true;
+        Message = message;
+        Data = data;
+    }
+
+    public Response(string message, List<ErrorModel> errors)
+    {
+        Succeeded = false;
+        Message = message;
+        Errors = errors;
+    }
+}

8ecaa470015e82649e3099155b7b636fe444bbf7
Shidlovsky Roman <shidlovskyroman@gmail.com>
2024-02-07 17:58:31 +0300

    Move swagger configuration to separate extension class

---
+++ WebApi/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebApi.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "Library Api",
+                Description = "Web API for simulating a library system (Create, Read, Update, Delete operations)."
+            });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Enter the Bearer Authorization string as following: `Bearer {token}.`",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+        });
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,4 @@
 using Application;
-using Application.Interfaces;
-using Application.Interfaces.Repositories;
-using Domain.Entities;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.OpenApi.Models;
 using Persistence;
 using Persistence.Context;
 using Persistence.Seed;
@@ -18,43 +13,10 @@ builder.Services.ConfigureApplication();
 builder.Services.ConfigureApiBehavior();
 builder.Services.ConfigureCorsPolicy();
 builder.Services.ConfigureJwtBearerAuthentication(builder.Configuration);
+builder.Services.ConfigureSwagger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Version = "v1",
-        Title = "Library Api",
-        Description = "Web API for simulating a library system (Create, Read, Update, Delete operations)."
-    });
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Description = "Enter the Bearer Authorization string as following: `Bearer {token}.`",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-                Reference = new OpenApiReference
-                {
-                    Id = "Bearer",
-                    Type = ReferenceType.SecurityScheme
-                }
-            },
-            new List<string>()
-        }
-    });
-});
 
 var app = builder.Build();
 
